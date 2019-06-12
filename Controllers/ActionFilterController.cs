0d5ab9e381c67d0689db8a9ef1a54d841e4f24ac
git-stephen <stephen280290@gmail.com>
2019-06-12 15:03:14 +0530

    Added action filter

---
+++ Controllers/ActionFilterController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using coremvcapp.Models;
+
+namespace coremvcapp.Controllers
+{
+    public class ActionFilterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Helper/ActionFilterHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using coremvcapp.Models;
+
+namespace coremvcapp.Helper
+{
+    public class ActionFilterHelper : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)       
+        {
+            //Inject some code to be executed before action start executing
+            
+            base.OnActionExecuting(filterContext); 
+        }
+        
+    }
+}
